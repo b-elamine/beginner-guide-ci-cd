ed09f0f2567df2427cd21bbdee9e5168033b901d
b-elamine <m.bechorfa@gmail.com>
2026-01-30 13:49:36 +0100

    adding app and test py files

--- test_app.py
+++ test_app.py
@@ -5,5 +5,5 @@ class TestApp(unittest.TestCase):
     def test_instruction(self):
         self.assertEqual(instruction(), "result")
 
-if __name__ == "__main__":
+if __name__ = "__main__":
     unittest.main()
